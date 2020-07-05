500f0be5fb984fcbc1ef878dbc1477608805e921
omarmoursi <omarshaaban911@gmail.com>
2020-07-05 08:19:48 +0200

    firist project

---
+++ medical_college.php
@@ -0,0 +1,122 @@
+<?php include('header.php'); ?>
+
+
+	<!-- mc info -->
+	<div class="form-group mc">
+		<h2 class="text-center" style="background-color:#272327;color: #fff;">Public Medical College in Bangladesh</h2>
+
+                              <table cellpadding="10" cellspacing="10" class="table table-hover">   
+
+                                  <tr>
+                                      <th>No.</th><th>Name</th><th>Acronym</th><th>Established</th><th>Location</th><th>website</td>
+                                  </tr>
+                                  <tr>
+                                      <td>01</td><td>Dhaka Medical College</td><td>DMC</td><td>1946</td><td>Dhaka</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>02</td><td>Sir Salimullah Medical College</td><td>SSMC</td><td>1972</td><td>Dhaka</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>03</td><td>Shaheed Suhrawardy Medical College</td><td>ShSMC</td><td>2006</td><td>Dhaka</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>04</td><td>Mymensingh Medical College</td><td>MMC</td><td>1962</td><td>Mymensingh</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>05</td><td>Chittagong Medical College</td><td>CMC</td><td>1957</td><td>Chittagong</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>06</td><td>Rajshahi Medical College</td><td>RMC</td><td>1958</td><td>Rajshahi</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>07</td><td>Sylhet MAG Osmani Medical College</td><td>SOMC</td><td>1962</td><td>Sylhet</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>08</td><td>Sher-e-Bangla Medical College</td><td>SBMC</td><td>1968</td><td>Barisal</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>09</td><td>Rangpur Medical College</td><td>RpMC</td><td>1970</td><td>Rangpur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>10</td><td>Comilla Medical College </td><td>CoMC</td><td>1992</td><td>Comilla</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>11</td><td>Khulna Medical College</td><td>KMC</td><td>1992</td><td>Khulna</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>12</td><td>Shaheed Ziaur Rahman Medical College</td><td>SZMC</td><td>1992</td><td>Bogra</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>13</td><td>Faridpur Medical College</td><td>FMC</td><td>1992</td><td>Faridpur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>14</td><td>Dinajpur  Medical College</td><td>DjMC</td><td>1992</td><td>Dinajpur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>15</td><td>Pabna Medical College</td><td>PMC</td><td>2008</td><td>Pabna</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>16</td><td>Abdul Malek Ukil Medical College</td><td>AMUMC</td><td>2008</td><td>Noakhali</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>17</td><td>Cox's Bazar Medical College</td><td>CoxMC</td><td>2008</td><td>Cox's Bazar</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>18</td><td>Jessore Medical College</td><td>JMC</td><td>2010</td><td>Jessore</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>19</td><td>Satkhira Medical College</td><td>SMC</td><td>2011</td><td>Satkhira</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>20</td><td>Shahid Syed Nazrul Islam  Medical College</td><td>SSNIMC</td><td>2011</td><td>Kishoreganj  </td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>21</td><td>Kushtia Medical College</td><td>KuMC</td><td>2011</td><td>Kushtia</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>22</td><td>Sheikh Sayera Khatun  Medical College</td><td>SSKMC</td><td>2011</td><td>Gopalganj</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>23</td><td>Shaheed Tajuddin Ahmad  Medical College</td><td>STAMC</td><td>2013</td><td>Gazipur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>24</td><td>Tangail Medical College</td><td>TMC</td><td>2014</td><td>Tangail</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>25</td><td>Jamalpur Medical College</td><td>JpMC</td><td>2014</td><td>Jamalpur</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>26</td><td>Colonel Malek Medical College</td><td>CMMMC</td><td>2014</td><td>Manikganj </td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>27</td><td>Shaheed M. Monsur Ali  Medical College</td><td>SMMAMC</td><td>2014</td><td>Sirajganj</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>28</td><td>Patuakhali Medical College</td><td>PkMC</td><td>2014</td><td>Patuakhali</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>29</td><td>Rangamati Medical College</td><td>RmMC</td><td>2014</td><td>Rangamati</td><td><a href="#">website</a></td>
+                                  </tr>
+                                  <tr>
+                                      <td>30</td><td>Mugda Medical College</td><td>MuMC</td><td>2015</td><td>Mugda</td><td><a href="#">website</a></td>
+                                  </tr>
+
+                                  
+                              </table>
+	</div>
+	
+
+    <!-- footer section --> 
+			 <?php include('footer.php'); ?>
+		<!-- footer section Ends--> 
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
